ed436f19799afac986845085311562e190ff0e16
dulal <dulalmiah70112@gmail.com>
2023-01-17 14:20:04 +0600

    backend

---
+++ index.js
@@ -0,0 +1,128 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+var mysql = require('mysql');
+const port = 4000
+
+app.use(cors());
+app.use(express.json())
+
+const con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database:"pase"
+  });
+  
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+  });
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.post('/product' , (req , res)=>{
+
+   const sql = 'insert into product set ?';
+   con.query(sql , req.body , (err,result)=>{
+    if(err){
+       console.log(err);
+    }
+    else{
+        res.send("successfully added")
+    }
+   })
+
+})
+
+
+app.post('/newUser',(req,res)=>{
+
+  const data = req.body;
+
+  const sql = 'insert into users set ?';
+  con.query(sql,data,(err,result)=>{
+
+    if(err){
+      console.log(err);
+    }
+    else{
+      res.send("success")
+    }
+  })
+})
+
+
+app.post('/admin' , (req,res)=>{
+
+  const emailClient = req.body.email;
+  const passwordClient = req.body.password;
+
+  const sql = `select * from admin where email = '${emailClient}' && password = '${passwordClient}'`;
+
+  con.query(sql,(err,result)=>{
+
+    if(err) throw err;
+    else{
+        if(result.length>0){
+          res.send(true)
+        }
+        else{
+          res.send(false);
+        }
+    }
+  })
+})
+
+
+app.get('/get' , (req , res) =>{
+
+    const sql = 'select * from product';
+
+    con.query(sql , (err , result)=>{
+
+        if(err) throw err;
+        else{
+            res.send(result)
+        }
+    })
+})
+
+app.get('/singleProduct/:id' , (req,res)=>{
+
+  const sql = `select * from product where id = '${req.params.id}'`;
+  con.query(sql , (err , result) =>{
+    if(err){
+
+      throw err;
+    }
+    else{
+      res.send(result)
+    }
+  })
+})
+
+app.get('/user/:user' , (req,res)=>{
+
+  const sql = `select * from users where id = '${req.params.user}'`;
+  con.query(sql , (err , result) =>{
+    if(err){
+
+      throw err;
+    }
+    else{
+      res.send(result)
+    }
+  })
+})
+
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
