5eff7c684bc8b64e4df9cd68d04828241f298ace
Dulal-12 <dulalmiah70112@gmail.com>
2023-02-22 12:32:50 +0600

    payment system add

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const SSLCommerzPayment = require('sslcommerz-lts')
 var mysql = require('mysql');
 const port = 4000
 
 app.use(cors());
+// app.use(cors({
+//   origin: 'http://localhost:3000/'
+// }))
 app.use(express.json())
+
+
 
 const con = mysql.createConnection({
     host: "localhost",
@@ -19,6 +25,13 @@ const con = mysql.createConnection({
     console.log("Connected!");
   });
 
+  //payment
+
+const id = 'pase63f54765b0185';
+const password = 'pase63f54765b0185@ssl';
+const sslcz = new SSLCommerzPayment(id,password, false);
+
+
 
 
 app.get('/', (req, res) => {
@@ -26,12 +39,29 @@ app.get('/', (req, res) => {
 })
 
 
+
+
 /*call the get api for all products information
 this is going to the products component
 */
-app.get('/get' , (req , res) =>{
+app.get('/get/:option' , (req , res) =>{
 
-  const sql = 'select * from product';
+  const op = req.params.option;
+  console.log(typeof(op))
+  if(op === '0'){
+
+    const sql = 'select * from product';
+    con.query(sql , (err , result)=>{
+
+      if(err) throw err;
+      else{
+          res.send(result)
+      }
+  })
+  }
+
+  else{
+  const sql = `select * from product where option = '${req.params.option}'`;
 
    con.query(sql , (err , result)=>{
 
@@ -40,6 +70,8 @@ app.get('/get' , (req , res) =>{
           res.send(result)
       }
   })
+}
+
 })
 
 
@@ -315,7 +347,7 @@ app.get('/userproduct' , (req , res) =>{
 
 app.get('/category/:data',(req,res)=>{
 
-  console.log(req.params.data)
+  //.log(req.params.data)
 })
 
 app.post('/taka' , (req , res)=>{
@@ -331,7 +363,132 @@ app.post('/taka' , (req , res)=>{
      res.send(result)
     }
   })
+})
 
+
+
+
+app.get('/create-session/:data', (req, res) => {
+
+  const arr = req.params.data.split(',');
+  const email = arr[0];
+  const money = arr[1];
+  const taka = arr[2]
+  const number = arr[3];
+  const data = {
+    total_amount: taka,
+    currency: 'BDT',
+    tran_id: 'REF123', // use unique tran_id for each api call
+    success_url: 'http://localhost:4000/ssl-payment-success',
+    fail_url: 'http://localhost:3000/ssl-payment-fail',
+    cancel_url: 'http://localhost:3000/ssl-payment-cancel',            
+    shipping_method: 'Courier',
+    product_name: 'Computer.',
+    product_category: 'Electronic',
+    product_profile: 'general',
+    cus_name: 'Customer Name',
+    cus_email: email,
+    cus_add1: 'Dhaka',
+    cus_add2: 'Dhaka',
+    cus_city: 'Dhaka',
+    cus_state: 'Dhaka',
+    cus_postcode: '1000',
+    cus_country: 'Bangladesh',
+    cus_phone: number,
+    cus_fax: '01711111111',
+    ship_name: 'Customer Name',
+    ship_add1: 'Dhaka',
+    ship_add2: 'Dhaka',
+    ship_city: 'Dhaka',
+    ship_state: 'Dhaka',
+    ship_postcode: 1000,
+    ship_country: 'Bangladesh',
+};
+
+  
+  const sql = `update validuser set taka='${money}' where email = '${email}'`;
+  con.query(sql,(err , result)=>{
+
+    if(err) throw err;
+    else{
+     //res.send(result)
+     sslcz.init(data).then(apiResponse => {
+      // Redirect the user to payment gateway
+      let GatewayPageURL = apiResponse.GatewayPageURL
+      // res.writeHead(301, {
+      //   Location: GatewayPageURL
+      // }).end();
+    // 
+    
+    //res.send(GatewayPageURL)
+      res.send({GatewayPageURL})
+     // res.redirect(GatewayPageURL)
+      //console.log('Redirecting to: ', GatewayPageURL)
+  });
+    }
+  })
+})
+
+
+app.post("/ssl-payment-notification", async (req, res) => {
+
+  /** 
+  * If payment notification
+  */
+
+  return res.status(200).json(
+    {
+      data: req.body,
+      message: 'Payment notification'
+    }
+  );
+})
+
+app.post("/ssl-payment-success", async (req, res) => {
+
+  /** 
+  * If payment successful 
+  */
+
+  // return res.status(200).json(
+  //   {
+  //     data: req.body,
+  //     message: 'Payment success'
+  //   }
+  // );
+
+  return res.redirect('http://localhost:3000/profile');
+})
+
+app.post("/ssl-payment-fail", async (req, res) => {
+
+  /** 
+  * If payment failed 
+  */
+
+  // return res.status(200).json(
+  //   {
+  //     data: req.body,
+  //     message: 'Payment failed'
+  //   }
+  // );
+  return res.redirect('http://localhost:3000/profile');
+})
+
+app.post("/ssl-payment-cancel", async (req, res) => {
+
+  /** 
+  * If payment cancelled 
+  */
+
+  // return res.status(200).json(
+  //   {
+  //     data: req.body,
+  //     message: 'Payment cancelled'
+  //   }
+  // );
+
+  return res.redirect('http://localhost:3000/profile');
 })
 
 app.listen(port, () => {
